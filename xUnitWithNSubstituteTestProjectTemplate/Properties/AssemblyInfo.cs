5b8e735f66ef6686494faed2bdb39bb03ef3d0a5
Lazaro <alfonso.lazaro@onmobile.com>
2020-09-18 18:49:40 +0200

    Renamed project. Add NSubstitute and FluentAssertions.

--- xUnitWithNSubstituteTestProjectTemplate/Properties/AssemblyInfo.cs
+++ xUnitWithNSubstituteTestProjectTemplate/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("xUnitTestProjectTemplate")]
+[assembly: AssemblyTitle("xUnitWithNSubstituteTestProjectTemplate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("J.Sakamoto")]
-[assembly: AssemblyProduct("xUnitTestProjectTemplate")]
+[assembly: AssemblyProduct("xUnitWithNSubstituteTestProjectTemplate")]
 [assembly: AssemblyCopyright("Copyright © J.Sakamoto 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
---
+++ xUnitWithNSubstituteTestProjectTemplate/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using NSubstitute;
+using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using FluentAssertions;
+using System;
+using System.Linq;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace $safeprojectname$
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test()
+        {
+        }
+    }
+}
